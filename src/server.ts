7b71f0262f716504c99e78417d38568331853a09
Ricardo da SilvaReis <oricardo_@hotmail.com>
2023-02-24 13:53:09 -0300

    Add swagger in project

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express"
+
+import swaggerFile from "./swagger.json"
 import cors from "cors";
 import "./database"
 import { router } from "./routes";
@@ -6,6 +9,8 @@ import "./shared/container";
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
